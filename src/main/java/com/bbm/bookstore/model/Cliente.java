883c7ad3431cb1047cab7c91c1fa78df0f825cf0
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-13 17:28:45 +0200

    adicionando o equals e hashCode

--- src/main/java/com/bbm/bookstore/model/Cliente.java
+++ src/main/java/com/bbm/bookstore/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.bbm.bookstore.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -45,6 +47,23 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
