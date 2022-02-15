21a4a0027b645a8da57182404a7d80e4dce08959
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-15 20:54:06 +0200

    liberando o CORS

--- src/main/java/com/bbm/bookstore/BookStoreApiApplication.java
+++ src/main/java/com/bbm/bookstore/BookStoreApiApplication.java
@@ -2,12 +2,22 @@ package com.bbm.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BookStoreApiApplication {
+public class BookStoreApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApiApplication.class, args);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// Libera o acesso aos End-Points da API
+		registry.addMapping("/**")
+		.allowedMethods("*")// libera quais metodos https(GET, POST, etc) podem ser acessados
+				.allowedOrigins("*");// libera o acesso para requisicoes de outros servers
 	}
 
 }
