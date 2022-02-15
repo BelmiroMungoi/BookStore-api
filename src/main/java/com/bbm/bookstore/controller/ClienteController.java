2bd41f2e60251e5b7fb2c51bbf22c971cc8bcd69
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-15 10:25:59 +0200

    Criando os end-points para atualizar e listar

--- src/main/java/com/bbm/bookstore/controller/ClienteController.java
+++ src/main/java/com/bbm/bookstore/controller/ClienteController.java
@@ -1,8 +1,11 @@
 package com.bbm.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,21 @@ public class ClienteController {
 		Cliente saveCliente = clienteRepository.save(cliente);
 		
 		return new ResponseEntity<Cliente>(saveCliente, HttpStatus.CREATED);
+	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Cliente>> listar(){
+		List<Cliente> clientes = clienteRepository.findAll();
+		
+		return new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<?> atualizar(@RequestBody Cliente cliente) {
+		if (cliente.getId() == null) {
+			return new ResponseEntity<String>("Cliente não encontrado! TENTE NOVAMENTE", HttpStatus.NOT_FOUND);
+		}
+		Cliente update = clienteRepository.saveAndFlush(cliente);
+		
+		return new ResponseEntity<Cliente>(update, HttpStatus.OK);
 	}
 }
