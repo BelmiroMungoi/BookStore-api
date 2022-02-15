ae26e25b436b4769b2ae93836d6185be47a39f8d
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-15 10:33:12 +0200

    adicionando um end-point para deletar cliente

--- src/main/java/com/bbm/bookstore/controller/ClienteController.java
+++ src/main/java/com/bbm/bookstore/controller/ClienteController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,7 @@ public class ClienteController {
 		return new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
 	}
 	
+	@PutMapping("/")
 	public ResponseEntity<?> atualizar(@RequestBody Cliente cliente) {
 		if (cliente.getId() == null) {
 			return new ResponseEntity<String>("Cliente não encontrado! TENTE NOVAMENTE", HttpStatus.NOT_FOUND);
@@ -42,5 +46,17 @@ public class ClienteController {
 		Cliente update = clienteRepository.saveAndFlush(cliente);
 		
 		return new ResponseEntity<Cliente>(update, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletar(@PathVariable("id") Long id){
+		
+		if(!clienteRepository.existsById(id)) {
+			return new ResponseEntity<String>("Cliente não encontrado ou já foi deletado", HttpStatus.NOT_FOUND);
+		}
+		
+		clienteRepository.deleteById(id);
+		
+		return new ResponseEntity<String>("Cliente Deletado com Sucesso", HttpStatus.OK);
 	}
 }
