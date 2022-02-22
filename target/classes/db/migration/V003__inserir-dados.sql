4d377eef14e10349e99634494deac2c1c7883edc
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-22 20:54:05 +0200

    inserindo as provincias e os seus distritos

---
+++ target/classes/db/migration/V003__inserir-dados.sql
@@ -0,0 +1,29 @@
+/*Insert de provincias*/
+INSERT INTO provincias VALUES (1, 'Maputo');
+INSERT INTO provincias VALUES (2, 'Maputo Cidade');
+INSERT INTO provincias VALUES (3, 'Gaza');
+INSERT INTO provincias VALUES (4, 'Inhambane');
+INSERT INTO provincias VALUES (5, 'Sofala');
+INSERT INTO provincias VALUES (6, 'Manica');
+INSERT INTO provincias VALUES (7, 'Tete');
+INSERT INTO provincias VALUES (8, 'Zambézia');
+INSERT INTO provincias VALUES (9, 'Nampula');
+INSERT INTO provincias VALUES (10, 'Cabo Delgado');
+INSERT INTO provincias VALUES (11, 'Niassa');
+
+/*Insert de distritos*/
+INSERT INTO distritos VALUES (1, 'Boane', 1);
+INSERT INTO distritos VALUES (2, 'Magude', 1);
+INSERT INTO distritos VALUES (3, 'Manhiça', 1);
+INSERT INTO distritos VALUES (4, 'Marracuene', 1);
+INSERT INTO distritos VALUES (5, 'Matola', 1);
+INSERT INTO distritos VALUES (6, 'Matutuíne', 1);
+INSERT INTO distritos VALUES (7, 'Moamba', 1);
+INSERT INTO distritos VALUES (8, 'Namaacha', 1);
+INSERT INTO distritos VALUES (9, 'KaMpfumo', 2);
+INSERT INTO distritos VALUES (10, 'Nlhamankulu', 2);
+INSERT INTO distritos VALUES (11, 'KaMaxaquene', 2);
+INSERT INTO distritos VALUES (12, 'KaMavota', 2);
+INSERT INTO distritos VALUES (13, 'KaMubukwana', 2);
+INSERT INTO distritos VALUES (14, 'KaTembe', 2);
+INSERT INTO distritos VALUES (15, 'KaNyaka', 4);
