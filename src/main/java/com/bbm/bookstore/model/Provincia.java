aec4eca6cee717a4cab762948f319e2b6b48eca8
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-23 16:28:02 +0200

    adicionando o modelo para provincias

---
+++ src/main/java/com/bbm/bookstore/model/Provincia.java
@@ -0,0 +1,49 @@
+package com.bbm.bookstore.model;
+
+import java.util.Objects;
+
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+public class Provincia {
+
+	@Id
+	private Long id;
+
+	@Size(max = 50)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Provincia other = (Provincia) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
