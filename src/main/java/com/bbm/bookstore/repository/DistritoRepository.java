40ea9f8d8b4990324f9847ec0a72aa991c6b3727
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-05-16 22:13:45 +0200

    criando a query para a busca dos distritos

--- src/main/java/com/bbm/bookstore/repository/DistritoRepository.java
+++ src/main/java/com/bbm/bookstore/repository/DistritoRepository.java
@@ -1,6 +1,9 @@
 package com.bbm.bookstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bbm.bookstore.model.Distritos;
@@ -8,4 +11,6 @@ import com.bbm.bookstore.model.Distritos;
 @Repository
 public interface DistritoRepository extends JpaRepository<Distritos, Long>{
 
+	@Query(nativeQuery = true, value = "select nome from distritos where provincia_id = ?1")
+	List<Distritos> findDistritoById(Long id);
 }
